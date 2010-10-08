cd4264d67843a591e220a69b2c19ffb4c7e5d572
Antonio Pardo <antonio.pardo@gmail.com>
2010-10-08 08:09:26 +0200

    ejemplo de vista y template

--- liberweb/serie/views.py
+++ liberweb/serie/views.py
@@ -1,10 +1,14 @@
+from liberweb.serie.models import Serie
+from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Listado de series')
+    series = Serie.objects.all()
+    return render_to_response('serie/index.html', {'series': series})
 
 def get_serie(request, serie_slug):
-    return HttpResponse("Hola, soy la serie: %s" % serie_slug)
+    serie = get_object_or_404(Serie, slug_name=serie_slug)
+    return render_to_response('serie/get_serie.html', {'serie': serie})
 
 def get_episodes(request, serie_slug):
     return HttpResponse('Listado de episodios')
---
+++ liberweb/templates/404.html
@@ -0,0 +1,1 @@
+<h1>Página no encontrada</h1>
