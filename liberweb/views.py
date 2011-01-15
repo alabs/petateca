0c183679a517cfd9eeae05a3c3d358fe52e8dab7
anders <anders@sindominio.net>
2011-01-15 18:37:26 +0000

    cleaning view

--- liberweb/views.py
+++ liberweb/views.py
@@ -1,22 +1,15 @@
 from liberweb.serie.models import Serie, Episode, Link
 from liberweb.blog.models import Post
-from django.contrib import messages
 from liberweb.decorators import render_to
-from liberweb.settings import SITE_NAME
 
 @render_to('index.html')
 def index(request):
-    post_list = Post.objects.all().order_by('-date')[:3]
-    post_list_2 = Post.objects.all().order_by('-date')[3:6]
     serie_list = Serie.objects.order_by('?')[:5]  # ?=random
     count_link = Link.objects.all().count()
     count_episode = Episode.objects.all().count()
     count_serie = Serie.objects.all().count()
     index_response = {
-                'posts': post_list,
-                'posts2': post_list_2,
                 'series': serie_list,
-                'title': SITE_NAME,
                 'count_link': count_link,
                 'count_episode': count_episode,
                 'count_serie': count_serie,
