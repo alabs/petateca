6c41a1064915a8364e56e1b9cdac0786696d45c9
anders <anders@sindominio.net>
2011-01-15 15:28:25 +0000

    Conteos generales de serie, episodio y links

--- liberweb/views.py
+++ liberweb/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render_to_response
-from liberweb.serie.models import Serie
+from liberweb.serie.models import Serie, Episode, Link
 from liberweb.blog.models import Post
 from django.contrib import messages
 from liberweb.decorators import render_to
@@ -10,20 +9,23 @@ def index(request):
     post_list = Post.objects.all().order_by('-date')[:3]
     post_list_2 = Post.objects.all().order_by('-date')[3:6]
     serie_list = Serie.objects.order_by('?')[:5]  # ?=random
-    if request.session.get('visited', False):
-        return {
+    count_link = Link.objects.all().count()
+    count_episode = Episode.objects.all().count()
+    count_serie = Serie.objects.all().count()
+    index_response = {
                 'posts': post_list,
                 'posts2': post_list_2,
                 'series': serie_list,
                 'title': SITE_NAME,
+                'count_link': count_link,
+                'count_episode': count_episode,
+                'count_serie': count_serie,
          }
+    if request.session.get('visited', False):
+        return index_response
     else:
         request.session['visited'] = True
         initial_message = 1
-        return {
-                'posts': post_list,
-                'posts2': post_list_2,
-                'series': serie_list,
+        return index_response.update({
                 'initial_message': initial_message,
-		'title': SITE_NAME,
-         }
+         })
