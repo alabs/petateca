6096bc237264ee72de5cc53ad62dd18fd67a3026
anders <anders@sindominio.net>
2011-03-10 14:06:23 +0100

    Se puede buscar por n caracteres, no hay minimo

--- petateca/search/views.py
+++ petateca/search/views.py
@@ -9,10 +9,8 @@ def search_lookup(request):
     if request.method == "GET":
         if request.GET.has_key(u'query'):
             value = request.GET[u'query']
-            # Ignore queries shorter than length 3
-            if len(value) > 1:
-                model_results = Serie.objects.filter(name__icontains=value)
-                suggestions = [ str(x.name) for x in model_results ]
-                #result = '{ query: %s, suggestions: %s }' % (value, suggestions)
+            model_results = Serie.objects.filter(name__icontains=value)
+            suggestions = [ str(x.name) for x in model_results ]
+            #result = '{ query: %s, suggestions: %s }' % (value, suggestions)
     json = simplejson.dumps({'query': value, 'suggestions': suggestions, })
     return HttpResponse(json, mimetype='application/json')
