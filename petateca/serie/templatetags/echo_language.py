f1749404a1adbaf0f2ded5a06719169ec5f258b8
Andres Pereira <apereira@alabs.es>
2011-04-11 16:31:55 +0200

    Recibe es, traduce a espaniol; recibe en, traduce a ingles

---
+++ petateca/serie/templatetags/echo_language.py
@@ -0,0 +1,20 @@
+# encoding: utf-8
+from django import template
+#from django.utils.translation import ugettext_lazy as _
+#TODO: i18n
+
+register = template.Library()
+
+
+@register.filter()
+def echo_language(value):  # Only one argument.
+    "Converts iso_code to language name"
+    lang = value
+    if lang == 'es' or lang == 'es-es': lang_name = 'Español'
+    elif lang == 'en': lang_name = 'Inglés'
+    elif lang == 'eu': lang_name = 'Euskera'
+    elif lang == 'ca': lang_name = 'Catalán'
+    elif lang == 'jp': lang_name = 'Japonés'
+    else: 
+        lang_name = 'Desconocido'
+    return lang_name
