3c0a4b20fb2116051948707534f759af9e67bd3d
Andres Pereira <apereira@alabs.es>
2011-04-14 18:26:15 +0200

    Arreglando UTF-8 en lookup (busqueda AJAX)

--- petateca/search/views.py
+++ petateca/search/views.py
@@ -10,7 +10,7 @@ def search_lookup(request):
         if request.GET.has_key(u'query'):
             value = request.GET[u'query']
             model_results = Serie.objects.filter(name__icontains=value)
-            suggestions = [ str(x.name) for x in model_results ]
+            suggestions = [ x.name for x in model_results ]
             #result = '{ query: %s, suggestions: %s }' % (value, suggestions)
     json = simplejson.dumps({'query': value, 'suggestions': suggestions, })
     return HttpResponse(json, mimetype='application/json')
