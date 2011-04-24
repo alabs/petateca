530c9a3604c996e27538688b2ffed2db9df6fed1
Andres Pereira <apereira@alabs.es>
2011-04-24 19:57:26 +0200

    Mejorando api

--- petateca/apps/api/urls.py
+++ petateca/apps/api/urls.py
@@ -1,8 +1,15 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('api.views',
-    url(r'^series$', 'series_list'),
-    url(r'^serie/(?P<id_serie>\d+)$', 'serie_detail'),
-    url(r'^serie/(?P<id_serie>\d+)/S(?P<season>\d+)$', 'season_detail'),
-    url(r'^serie/(?P<id_serie>\d+)/S(?P<season>\d+)E(?P<episode>\d+)$', 'episode_detail'),
+    url(r'^/series$', 'series_list'),
+    url(r'^/serie/(?P<id_serie>\d+)$', 'serie_detail', name='API_serie_detail'),
+    url(r'^/serie/(?P<id_serie>\d+)/S(?P<season>\d+)$', 'season_detail', name='API_season_detail'),
+    url(r'^/serie/(?P<id_serie>\d+)/S(?P<season>\d+)E(?P<episode>\d+)$', 'episode_detail', name='API_episode_detail'),
+)
+
+urlpatterns += patterns('',
+    url(r'^$', 'django.views.generic.simple.direct_to_template',
+        {'template': 'api.html'},
+        name="api"
+       ),
 )
