7660e91ab47cc90d1ff5f89e2fdc4e7ca57e808c
Andres Pereira <apereira@alabs.es>
2011-05-04 15:28:20 +0200

    Ordenando URL en ajax

--- petateca/apps/serie/urls/entity.py
+++ petateca/apps/serie/urls/entity.py
@@ -27,12 +27,16 @@ urlpatterns += patterns('serie.ajax',
     url(r'^lookup/serie/(?P<serie_id>[-\w]+)$', 'serie_lookup'),
     url(r'^lookup/serie/(?P<serie_id>[-\w]+)/season/(?P<season>\d+)$', 'season_lookup'),
     url(r'^lookup/actors/(?P<serie_slug>[-\w]+)$', 'actors_lookup'),
+
+    # AJAX /series
     url(r'^lookup/letter/(?P<letter>[-\w]+)$', 'ajax_letter'),
     url(r'^lookup/genre/(?P<genre_slug>[-\w]+)$', 'ajax_genre'),
     url(r'^lookup/network/(?P<network_slug>[-\w]+)$', 'ajax_network'),
-    url(r'^lookup/links/(?P<serie_id>[-\w]+)/(?P<season>\d+)/(?P<episode>\d+)/$', 'links_episode_lookup', name='ajax_links_episodes'),
-    url(r'^lookup/links/(?P<serie_id>[-\w]+)/season/(?P<season>\d+)/$', 'links_season_lookup', name='ajax_links_season'),
+
+    # AJAX /serie/nombre
     url(r'^lookup/espoiler/(?P<serie_id>[-\w]+)/(?P<season>\d+)/(?P<episode>\d+)/$', 'espoiler_lookup', name='ajax_espoiler'),
     url(r'^lookup/vote/$', 'vote_link', name='vote_link'),
+    url(r'^lookup/links/(?P<serie_id>[-\w]+)/(?P<season>\d+)/(?P<episode>\d+)/$', 'links_episode_lookup', name='ajax_links_episodes'),
+    url(r'^lookup/links/(?P<serie_id>[-\w]+)/season/(?P<season>\d+)/$', 'links_season_lookup', name='ajax_links_season'),
     url(r'^lookup/add_link/(?P<episode_id>\d+)/$', 'ajax_add_link', name='ajax_add_link'),
 )
