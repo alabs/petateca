9b3555c99a80aff6a22131a68444ca96eaa200e0
Andres Pereira <apereira@alabs.es>
2011-05-07 04:07:10 +0200

    nuevo autocomplete

--- petateca/apps/search/views.py
+++ petateca/apps/search/views.py
@@ -2,15 +2,50 @@ from django.utils import simplejson
 from django.http import HttpResponse
 from serie.models import Serie
 
+from django.db.models import Q
+from decorators import render_to
 
+@render_to('search/ajax_search.html')
 def search_lookup(request):
+    ''' 
+    Busqueda en AJAX con jQuery 
+    Se siguio este tutorial: http://www.marcofolio.net/webdesign/a_fancy_apple.com-style_search_suggestion.html
+    (Adaptandolo a django)
+
+    TODO: Agregarle navegacion con teclado: http://www.johnboy.com/blog/tutorial-instant-search-with-arrow-key-navigation-using-jquery-and-php
+
+    Escupe los resultados en este formato:
+
+    <p id="searchresults">
+       <span class="category">[CATEGORY_FROM_DATABASE]</span>
+          <a href="[URL_FROM_DATABASE]">
+             <img alt="" src="search_images/[IMG_FROM_DATABASE]"/>
+             <span class="searchheading">[HEADING_FROM_DATABASE]</span>
+             <span>[TEXT_FROM_DATABASE]</span>
+          </a>
+          <a href="[URL_FROM_DATABASE]">
+             <img alt="" src="search_images/[IMG_FROM_DATABASE]"/>
+             <span class="searchheading">[HEADING_FROM_DATABASE]</span>
+             <span>[TEXT_FROM_DATABASE]</span>
+          </a>
+          <!-- more items -->
+       <span class="category">Webdesign</span>
+          <a href="[URL_FROM_DATABASE]">
+             <img alt="" src="search_images/[IMG_FROM_DATABASE]"/>
+             <span class="searchheading">[HEADING_FROM_DATABASE]</span>
+             <span>[TEXT_FROM_DATABASE]</span>
+          </a>
+       <!-- more categories -->
+       <span class="seperator">
+          <a href="http://www.marcofolio.net/sitemap.html">Nothing interesting here? Try the sitemap.</a>
+       </span>
+    </p>
+    '''
     # Default return list
     results = []
     if request.method == "GET":
         if request.GET.has_key(u'query'):
             value = request.GET[u'query']
-            model_results = Serie.objects.filter(name__icontains=value)
-            suggestions = [ x.name for x in model_results ]
-            #result = '{ query: %s, suggestions: %s }' % (value, suggestions)
-    json = simplejson.dumps({'query': value, 'suggestions': suggestions, })
-    return HttpResponse(json, mimetype='application/json')
+            model_results = Serie.objects.filter( Q(name_es__icontains=value) | Q(name__icontains=value))
+            return  { 'results': model_results }
+
