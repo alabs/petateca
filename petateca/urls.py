8cc0ab8984a2cd3b0a3ba023295821d589f5abc7
Andres Pereira <apereira@alabs.es>
2011-06-04 17:58:14 +0200

    Se me paso la $ al final de api, se rompio, arreglado

--- petateca/urls.py
+++ petateca/urls.py
@@ -25,7 +25,7 @@ urlpatterns = patterns('',
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
 
-    url(r'^api/', redirect_to, { 'url': '/api/v1' }, name='api'),
+    url(r'^api/$', redirect_to, { 'url': '/api/v1' }, name='api'),
     (r'^api/v1/', include('api.urls')),
 
     (r'^search/', include('search.urls')),
