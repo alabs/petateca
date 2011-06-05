d91e606e822940c59761a01fd4484eee6e503b6a
Andres Pereira <apereira@alabs.es>
2011-06-05 12:28:39 +0200

    Cambios en la docu de la API v2

--- petateca/apps/api_v2/urls.py
+++ petateca/apps/api_v2/urls.py
@@ -15,7 +15,7 @@ season_resource = Resource(handler=h.SeasonHandler, **ad)
 episode_resource = Resource(handler=h.EpisodeHandler, **ad)
 
 urlpatterns = patterns('',
-    (r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'api.html'}),
+    (r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'api_v2.html'}),
     url(r'^series/$', serielist_resource, name='API_v2_serie_list'),
     url(r'^series/(?P<serie_id>\d+)/$', serie_resource, name='API_v2_serie_detail'),
     url(r'^series/(?P<serie_id>\d+)/seasons/$', seasonlist_resource, name='API_v2_season_list'),
