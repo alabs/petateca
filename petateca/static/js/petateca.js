20318ea93640536884a6289d88201362f892127a
Andres Pereira <apereira@alabs.es>
2011-06-14 10:48:38 +0200

    qtip: Pruebas con el modal de login/registro  y de cambio de avatar

--- petateca/static/js/petateca.js
+++ petateca/static/js/petateca.js
@@ -297,7 +297,36 @@ $(document).ready(function () {
         });
 
     // El popup cuando se hace click en login
-	$(".login").colorbox({iframe:true, innerWidth:555, innerHeight:324});
+//	$(".login").colorbox({iframe:true, innerWidth:555, innerHeight:324});
+  $('.login').each(function(){
+        $login = $(this);
+        $login.click( function(){ return false; }); 
+        $.get('/accounts/login/', function(data){
+            $login.qtip(
+            {
+                id: 'modal', // Since we're only creating one modal, give it an ID so we can style it
+                content: {
+                    text: data,
+                    title: {
+                        text: 'Iniciar sesión',
+                        button: true
+                    }
+                },
+                position: {
+                    my: 'center', // ...at the center of the viewport
+                    at: 'center',
+                    target: $(window)
+                },
+                show: {
+                    event: 'click', // Show it on click...
+                    solo: true, // ...and hide all other tooltips...
+                    modal: true // ...and make it modal
+                },
+                style: 'ui-tooltip-light ui-tooltip-rounded'
+            });
+            return false;
+        });
+     });
 
     // Que cuando se haga click en el rating envia el valor por post
     $('.ratingstar').rating({ callback: function(value, link){
@@ -329,13 +358,46 @@ $(document).ready(function () {
     });
 
     // Popup de añadir enlace
-    $('.add_link').colorbox({width:'50%', height:'60%', iframe:true, 
-         onClosed:function(){ location.reload(true); } 
-    });
+//    $('.add_link').colorbox({width:'50%', height:'60%', iframe:true, 
+//         onClosed:function(){ location.reload(true); } 
+//    });
+//
+//    // Popup de cambiar avatar
+//	$(".avatar_change").colorbox({width:"600px", height:"500px", iframe:true,
+//     onClosed:function(){ location.reload(true); } });
+//
 
-    // Popup de cambiar avatar
-	$(".avatar_change").colorbox({width:"600px", height:"500px", iframe:true,
-     onClosed:function(){ location.reload(true); } });
+  $('.avatar_change').each(function(){
+        $avatar = $(this);
+        $avatar.click( function(){ return false; }); 
+        $.get('/accounts/avatar/change/', function(data){
+            $avatar.qtip(
+            {
+                id: 'modal', // Since we're only creating one modal, give it an ID so we can style it
+                content: {
+                    text: data,
+                    title: {
+                        text: 'Cambiar avatar',
+                        button: true
+                    }
+                },
+                position: {
+                    my: 'center', // ...at the center of the viewport
+                    at: 'center',
+                    target: $(window)
+                },
+                show: {
+                    event: 'click', // Show it on click...
+                    solo: true, // ...and hide all other tooltips...
+                    modal: true // ...and make it modal
+                },
+                style: 'ui-tooltip-light ui-tooltip-rounded'
+            });
+            return false;
+        });
+     });
+
+
 
     // Popup de las series, cuando se pone encima de la imagen
    $('.serie').live('mouseover', function() {
