a8b3ffe7ea43d7e56098e7b76522433425c71199
Andres Pereira <apereira@alabs.es>
2011-06-14 20:26:34 +0200

    Cambio de avatar en la misma pagina

--- petateca/static/js/petateca.js
+++ petateca/static/js/petateca.js
@@ -405,35 +405,35 @@ $(document).ready(function () {
 //     onClosed:function(){ location.reload(true); } });
 //
 
-  $('.avatar_change').each(function(){
-        $avatar = $(this);
-        $avatar.click( function(){ return false; }); 
-        $.get('/accounts/avatar/change/', function(data){
-            $avatar.qtip(
-            {
-                id: 'modal', // Since we're only creating one modal, give it an ID so we can style it
-                content: {
-                    text: data,
-                    title: {
-                        text: 'Cambiar avatar',
-                        button: true
-                    }
-                },
-                position: {
-                    my: 'center', // ...at the center of the viewport
-                    at: 'center',
-                    target: $(window)
-                },
-                show: {
-                    event: 'click', // Show it on click...
-                    solo: true, // ...and hide all other tooltips...
-                    modal: true // ...and make it modal
-                },
-                style: 'ui-tooltip-light ui-tooltip-rounded'
-            });
-            return false;
-        });
-     });
+//  $('.avatar_change').each(function(){
+//        $avatar = $(this);
+//        $avatar.click( function(){ return false; }); 
+//        $.get('/accounts/avatar/change/', function(data){
+//            $avatar.qtip(
+//            {
+//                id: 'modal', // Since we're only creating one modal, give it an ID so we can style it
+//                content: {
+//                    text: data,
+//                    title: {
+//                        text: 'Cambiar avatar',
+//                        button: true
+//                    }
+//                },
+//                position: {
+//                    my: 'center', // ...at the center of the viewport
+//                    at: 'center',
+//                    target: $(window)
+//                },
+//                show: {
+//                    event: 'click', // Show it on click...
+//                    solo: true, // ...and hide all other tooltips...
+//                    modal: true // ...and make it modal
+//                },
+//                style: 'ui-tooltip-light ui-tooltip-rounded'
+//            });
+//            return false;
+//        });
+//     });
 
 
     // Popup de las series, cuando se pone encima de la imagen
