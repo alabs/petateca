45f2b3c0fd9fa9676b8ba717cad63b4625c16e89
Andres Pereira <apereira@alabs.es>
2011-08-04 22:28:14 +0200

    Soportando https en extract_domain

--- petateca/apps/serie/templatetags/extract_domain.py
+++ petateca/apps/serie/templatetags/extract_domain.py
@@ -8,10 +8,16 @@ register = template.Library()
 @stringfilter
 def extract_domain(value):  # Only one argument.
     "Extract domain from url"
+    domain = "WRONG URL???"
     try:
         domain = value.split('http://')[1].split('/')[0]
         if domain.startswith('www.'):
             domain = domain.replace('www.', '')
     except:
-        domain = "WRONG URL???"
+        try:
+            domain = value.split('https://')[1].split('/')[0]
+            if domain.startswith('www.'):
+                domain = domain.replace('www.', '')
+        except:
+            domain = "WRONG URL???"
     return domain
